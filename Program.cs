c71d68ea59312bf81e5f388dd338588d49185fec
Tridib Roy Arjo <softmetricltd@gmail.com>
2022-08-30 16:52:44 +0600

    Added UEH (Universal Error Handler)

--- Program.cs
+++ Program.cs
@@ -13,24 +13,24 @@ namespace MixInStyle
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //try
-            //{
+            try
+            {
                 Application.Run(new Mixer());
-            //}
+            }
 
-            //catch (Exception ex)
-            //{
-            //    Clipboard.SetText(ex.ToString());
-            //    if (MessageBox.Show("It looks like we ran into problem. The issue might be with corrupted settings or unseen bug. The error description has been copied to your clipboard. Do you wish to re-open the program after resetting the settings?", "Fatal RE", MessageBoxButtons.YesNo, MessageBoxIcon.Error) == DialogResult.Yes)
-            //    {
-            //        Core.Settings.Reset();
-            //        Application.Restart();
-            //    }
-            //    else
-            //    {
+            catch (Exception ex)
+            {
+                Clipboard.SetText(ex.ToString());
+                if (MessageBox.Show("It looks like we ran into problem. The issue might be with corrupted settings or unseen bug. The error description has been copied to your clipboard. Do you wish to re-open the program after resetting the settings?", "Fatal RE", MessageBoxButtons.YesNo, MessageBoxIcon.Error) == DialogResult.Yes)
+                {
+                    Core.Settings.Reset();
+                    Application.Restart();
+                }
+                else
+                {
 
-            //    }
-            //}
+                }
+            }
         }
     }
 }
